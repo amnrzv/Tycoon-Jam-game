23461a5ff6b8845b26e5f8b3fb45bad8dd061b04
Extra Eyes <generalistgames@gmail.com>
2017-03-06 02:39:09 -0500

    -Added clock functionality for some reason.

---
+++ Assets/Scripts/Components/Clock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+	public Transform minuteHand;
+	public Transform hourHand;
+	private IEnumerator coroutine;
+	void Start () 
+	{
+		coroutine = Wait(1);
+		StartCoroutine(coroutine);
+	}
+
+	IEnumerator Wait (int wait) 
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(wait);
+			minuteHand.transform.localEulerAngles = new Vector3(0, 0, TimeManager.time.minutes * 6);
+			hourHand.transform.localEulerAngles = new Vector3(0, 0, TimeManager.time.hours * 30);
+		}
+	}
+}
